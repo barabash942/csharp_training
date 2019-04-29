a4c4a5e15277eec13b99836db137dc8c0b1b1612
barabashe <Barabash.Eugenia@palex.ru>
2019-04-29 12:23:23 +0700

    Task 3. Added classes for AccountData and GroupData

--- Addressbook.Web.Tests/Addressbook.Web.Tests/Tests/GroupCreationTest.cs
+++ Addressbook.Web.Tests/Addressbook.Web.Tests/Tests/GroupCreationTest.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
+using System;
+using System.Text;
 
 namespace Addressbook.Web.Tests
 {
@@ -42,29 +39,73 @@ namespace Addressbook.Web.Tests
         [Test]
         public void GroupCreationCaseTest()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            GroupData group = new GroupData("Test1");
+            group.Header = "Text";
+            group.Footer = "another text";
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturnToGroupsPage()
+        {
             driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(GroupData groupdata)
+        {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("Test1");
+            driver.FindElement(By.Name("group_name")).SendKeys(groupdata.Name);
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("some text");
+            driver.FindElement(By.Name("group_header")).SendKeys(groupdata.Header);
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("another text");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
+            driver.FindElement(By.Name("group_footer")).SendKeys(groupdata.Footer);
         }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
--- Addressbook.Web.Tests/Addressbook.Web.Tests/Tests/UnitTest1.cs
+++ Addressbook.Web.Tests/Addressbook.Web.Tests/Tests/UnitTest1.cs
@@ -7,8 +7,35 @@ namespace Addressbook.Web.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSquare()
         {
+            Square s1 = new Square(10);
+            Square s2 = new Square(20);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 10);
+            Assert.AreEqual(s2.Size, 20);
+            Assert.AreEqual(s3.Size, 10);
+
+            s3.Size = 15;
+            Assert.AreEqual(s1.Size, 15);
+            s2.Colored = true;
+        }
+
+        [TestMethod]
+        public void TestMethodCircle()
+        {
+            Circle s1 = new Circle(10);
+            Circle s2 = new Circle(20);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 10);
+            Assert.AreEqual(s2.Radius, 20);
+            Assert.AreEqual(s3.Radius, 10);
+
+            s3.Radius = 15;
+            Assert.AreEqual(s1.Radius, 15);
+        
         }
     }
 }
