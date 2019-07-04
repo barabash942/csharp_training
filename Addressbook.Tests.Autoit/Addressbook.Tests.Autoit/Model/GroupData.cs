b8c026ffc4a72e0a114f43e3d44d7051a124d250
barabashe <Barabash.Eugenia@palex.ru>
2019-07-04 10:41:16 +0700

    Lecture 8.1

---
+++ Addressbook.Tests.Autoit/Addressbook.Tests.Autoit/Model/GroupData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addressbook.Tests.Autoit
+{
+    public class GroupData : IComparable<GroupData>, IEquatable<GroupData>
+    {
+    public string Name { get; set; }
+
+        public int CompareTo(GroupData other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public bool Equals(GroupData other)
+        {
+            return this.Name.Equals(other.Name);
+        }
+    }
+}
---
+++ Addressbook.Tests.Autoit/Addressbook.Tests.Autoit/Tests/TestBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Addressbook.Tests.Autoit
+{
+    public class TestBase
+    {
+        public ApplicationManager app;
+
+
+        [OneTimeSetUp]
+        public void initApplication()
+        {
+            app = new ApplicationManager();
+        }
+
+        [OneTimeTearDown]
+        public void stopApplication()
+        {
+            app.Stop();
+        }
+    }
+}
